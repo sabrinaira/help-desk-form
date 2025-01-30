7d51f608ab45ecdfc8dc0099282ed711fa61f9ca
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-01-30 02:40:24 -0500

    uninstalled roboto font package

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import 'react';
 import { Card, CardContent, Divider, Typography, Box } from '@mui/material';
 import HelpDeskForm from './HelpDesk';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 function App() {
   return (
     <Box sx={{ padding: '20px', maxWidth: 600, margin: 'auto' }}>
-      <Typography variant='h4' fontFamily='roboto' gutterBottom>
+      <Typography variant='h4' gutterBottom>
         Help Desk Ticket
       </Typography>
       <Card variant='outlined'>
         <CardContent>
-          <Typography variant='body1' fontFamily='roboto'>
+          <Typography variant='body1'>
             I made this simple template to write up a help desk ticket more
             efficiently!<Divider></Divider>
             Once you paste your text on Slack, use this following keyboard command to
