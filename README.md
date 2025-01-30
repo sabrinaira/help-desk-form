dd2630b27539e3742fb3cb458cb3fb0f15636c31
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-01-30 10:12:29 -0500

    updated README; added link to Github Repo within component

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# help-desk-form
+# Help Desk Form
 An easy way to create a help desk form to copy and paste!
+
+### <a href="https://sabrinaira.github.io/help-desk-form/" target="_blank">Click here to see the form!</a>
+
+---
+I made this quick Help Desk Form to copy and paste onto any communicative platform like Slack, so that it is more efficient to type up everything and copy the whole text onto your clipboard with the questions included.
+
+<img src="https://imgur.com/4wHl31Z.png" height="auto" width="90%">
+
+Once you paste your text on Slack , use this following keyboard command to automatically implement the formatting built on Slack: <br>
+Windows: `CTRL + Shift + F` <br>
+MacOS: `CMD + Shift + F`
+
+Or just paste it in plain text and use your preferred app's built-in formatting options to edit it there!
+
+---
+#### Built with:
+- Material UI
+- React
+- TypeScript
+- Vite
+
+---
+##### Special Note: If you are my cohort mate, you're welcome to fork and mess with it or improve it!
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import HelpDeskForm from './HelpDesk';
 function App() {
   return (
     <Box sx={{ padding: '20px', maxWidth: 600, margin: 'auto' }}>
-      <Typography variant='h4' gutterBottom>
+      <Typography variant='h4' align='center' gutterBottom>
         Help Desk Ticket
       </Typography>
       <Card variant='outlined'>
@@ -13,8 +13,8 @@ function App() {
           <Typography variant='body1'>
             I made this simple template to write up a help desk ticket more
             efficiently!<Divider></Divider>
-            Once you paste your text on Slack, use this following keyboard command to
-            automatically implement the formatting built on Slack:
+            Once you paste your text on Slack, use this following keyboard
+            command to automatically implement the formatting built on Slack:
             <pre>
               <code>CTRL/CMD + Shift + F</code>
             </pre>
@@ -23,6 +23,19 @@ function App() {
       </Card>
       <br></br>
       <HelpDeskForm />
+      <Typography align='center'>
+        <a
+          href='https://github.com/sabrinaira/help-desk-form'
+          style={{
+            textDecoration: 'none',
+            color: '#3f51b5',
+            padding: '10px',
+            fontSize: '16px',
+          }}
+        >
+          Click here to go back to Github Repository
+        </a>
+      </Typography>
     </Box>
   );
 }
